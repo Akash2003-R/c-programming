b7589d45c12ce4d17422fc5d23c429125875af43
unknown <4nn21cs@nieit.ac.in>
2022-10-21 15:31:44 +0530

    program1

---
+++ array.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+int a[10],n;
+printf("enter the number of elements in array");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+scanf("%d",&a[i]);
+printf("enter the elements in array");
+for(i=0;i<n;i++)
+printf("%d",a[i]);
+}
